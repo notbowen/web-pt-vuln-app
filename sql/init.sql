c61f9b748d00d34095596734aa0bc96a365f2857
Hu Bowen <contact@hubowen.dev>
2023-11-26 16:50:24 +0800

    feat: added user status

--- sql/init.sql
+++ sql/init.sql
@@ -4,7 +4,8 @@ USE vulnerable_db;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    status VARCHAR(255) DEFAULT "Closed Alpha Tester"
 );
 
-INSERT INTO users (username, password) VALUES ('John', 'f8IhYM7cUyXecSHAodQOqzqH7');
+INSERT INTO users (username, password, status) VALUES ('John', 'f8IhYM7cUyXecSHAodQOqzqH7', 'First Alpha tester!');
