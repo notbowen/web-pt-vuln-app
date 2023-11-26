af467f61c88c9150758cd1b410f5cdb12024eace
Hu Bowen <contact@hubowen.dev>
2023-11-26 18:31:56 +0800

    feat: vuln to SQLi

--- public/users.php
+++ public/users.php
@@ -1,25 +1,61 @@
 <?php
 session_start();
-include_once("../templates/header.php");
-include_once("../templates/navbar.php");
 include_once("../config/db.php");
 
-echo "<h1>Registered Users</h1>";
+// Initialize a variable to store the table content
+$tableContent = '';
+$isResultEmpty = true;
 
-// SQL query to select all users
-$sql = "SELECT username, status FROM users";
-$result = $conn->query($sql);
+// Define the query to fetch all users and an additional query for search
+$sql = "SELECT username, status FROM users;";
+if (isset($_GET['search']) && $_GET['search'] != '') {
+    $searchTerm = $_GET['search'];
+    $sql = "SELECT username, status FROM users WHERE username LIKE '%$searchTerm%';";
+}
 
-if ($result->num_rows > 0) {
-    // Output data of each row
-    echo "<table border='1'><tr><th>Username</th><th>Custom Status</th></tr>";
-    while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . htmlspecialchars($row["username"]) . "</td><td>" . htmlspecialchars($row["status"]) . "</td></tr>";
-    }
-    echo "</table>";
+// Execute multi query and prepare the table content
+if ($conn->multi_query($sql)) {
+    do {
+        if ($result = $conn->store_result()) {
+            if ($result->num_rows > 0) {
+                $isResultEmpty = false;
+                $tableContent .= "<table border='1'><tr><th>Username</th><th>Status</th></tr>";
+                while ($row = $result->fetch_assoc()) {
+                    $tableContent .= "<tr><td>" . htmlspecialchars($row["username"]) . "</td><td>" . htmlspecialchars($row["status"]) . "</td></tr>";
+                }
+                $tableContent .= "</table>";
+            }
+            $result->free();
+        }
+    } while ($conn->more_results() && $conn->next_result());
 } else {
-    echo "No users found";
+    $tableContent = "<p>Error executing query: " . $conn->error . "</p>";
 }
 
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Users</title>
+</head>
+<body>
+    <?php include_once("../templates/navbar.php"); ?>
+
+    <h2>Users</h2>
+    <form method="get">
+        <input type="text" name="search" placeholder="Enter username" value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
+        <input type="submit" value="Search">
+    </form>
+
+    <!-- Display the table content or a 'no results' message -->
+    <?php
+    if ($isResultEmpty) {
+        echo "<p>No users found.</p>";
+    } else {
+        echo "<br>" . $tableContent;
+    }
+    ?>
+</body>
+</html>
